dd77830d9f64ca7097ad5b4ad34bdffe3485cc58
Loni Moon <lonimua@MacBook-Air.local>
2026-04-11 02:17:16 -1000

    Set WebView background color to remove black startup gap

--- frontend/android/app/src/main/java/com/glimmerquest/game/MainActivity.java
+++ frontend/android/app/src/main/java/com/glimmerquest/game/MainActivity.java
@@ -1,5 +1,15 @@
 package com.glimmerquest.game;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Force WebView background to match app theme
+        getWindow().getDecorView().setBackgroundColor(0xFF0B0B13);
+    }
+}
